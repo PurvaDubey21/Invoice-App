adbf4cf96f44dcd60d9195b8d53320b6c05e59f7
pragyaDixit-coder <dpragya418@gmail.com>
2026-01-08 15:32:58 +0530

    changes

--- invoicing-frontend/src/components/common/PageHeader.tsx
+++ invoicing-frontend/src/components/common/PageHeader.tsx
@@ -36,7 +36,7 @@ export const PageHeader = ({
             textTransform: "none",
             fontSize: 12,
             padding: "4px 14px",
-            borderColor: "#d1d2d4",
+            borderColor: "#fff",
             ml:2
             
             
